81d1f3d661d18e001abd727f8627b6da174b8bc5
harris-ishaq <harris.ibrahim@gmail.com>
2021-11-01 17:30:52 +0800

    Add Find Place Function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"gmaps-location-test/pkg/gmaps"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -29,6 +30,18 @@ func LocationWithFilter(req gmaps.GeocodeReverseRequest) *response.GeneralRespon
 	return response.Success(200, resp)
 }
 
+func FindPlace(req gmaps.FindPlaceRequest) *response.GeneralResponse {
+	loc := strings.ReplaceAll(req.Location, " ", "%20")
+	log.Print(loc)
+	resp, err := gmaps.FindPlace(loc)
+	if err != nil {
+		log.Printf("[FindPlace] Error getting place info, cause %v", err)
+		return response.Error(500, "Internal Server Error.")
+	}
+
+	return response.Success(200, resp)
+}
+
 func LocationUser(c echo.Context) error {
 	body := new(gmaps.GeocodeReverseRequest)
 	c.Bind(body)
@@ -45,12 +58,21 @@ func LocationFiltered(c echo.Context) error {
 	return c.JSON(http.StatusOK, response)
 }
 
+func Place(c echo.Context) error {
+	body := new(gmaps.FindPlaceRequest)
+	c.Bind(body)
+
+	response := FindPlace(*body)
+	return c.JSON(http.StatusOK, response)
+}
+
 func main() {
 	log.SetFlags(log.Llongfile | log.Ldate | log.Ltime)
 	e := echo.New()
 
 	e.POST("/location", LocationUser)
 	e.POST("/location/filter", LocationFiltered)
+	e.POST("/find_place", Place)
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- pkg/gmaps/gmaps.go
+++ pkg/gmaps/gmaps.go
@@ -15,7 +15,13 @@ type (
 		Long string `json:"long"`
 	}
 
-	Response struct {
+	FindPlaceRequest struct {
+		Location string `json:"location"`
+	}
+)
+
+type (
+	ResponseGeocodeReverse struct {
 		PlusCode PlusCode  `json:"plus_code"`
 		Results  []Results `json:"results"`
 		Status   string    `json:"status"`
@@ -66,7 +72,73 @@ type (
 	}
 )
 
-func GeocodeReverse(req GeocodeReverseRequest) (*Response, error) {
+type (
+	ResponseFindPlace struct {
+		Candidates []Candidates `json:"candidates"`
+		Status     string       `json:"status"`
+	}
+
+	LocationFindPlace struct {
+		Lat float64 `json:"lat"`
+		Lng float64 `json:"lng"`
+	}
+
+	NortheastFindPlace struct {
+		Lat float64 `json:"lat"`
+		Lng float64 `json:"lng"`
+	}
+
+	SouthwestFindPlace struct {
+		Lat float64 `json:"lat"`
+		Lng float64 `json:"lng"`
+	}
+
+	ViewportFindPlace struct {
+		Northeast NortheastFindPlace `json:"northeast"`
+		Southwest SouthwestFindPlace `json:"southwest"`
+	}
+
+	GeometryFindPlace struct {
+		Location LocationFindPlace `json:"location"`
+		Viewport ViewportFindPlace `json:"viewport"`
+	}
+
+	OpeningHours struct {
+		OpenNow bool `json:"open_now"`
+	}
+
+	Photos struct {
+		Height           int      `json:"height"`
+		HTMLAttributions []string `json:"html_attributions"`
+		PhotoReference   string   `json:"photo_reference"`
+		Width            int      `json:"width"`
+	}
+
+	PlusCodeFindPlace struct {
+		CompoundCode string `json:"compound_code"`
+		GlobalCode   string `json:"global_code"`
+	}
+
+	Candidates struct {
+		BusinessStatus      string            `json:"business_status"`
+		FormattedAddress    string            `json:"formatted_address"`
+		Geometry            GeometryFindPlace `json:"geometry"`
+		Icon                string            `json:"icon"`
+		IconBackgroundColor string            `json:"icon_background_color"`
+		IconMaskBaseURI     string            `json:"icon_mask_base_uri"`
+		Name                string            `json:"name"`
+		OpeningHours        OpeningHours      `json:"opening_hours"`
+		Photos              []Photos          `json:"photos"`
+		PlaceID             string            `json:"place_id"`
+		PlusCode            PlusCodeFindPlace `json:"plus_code"`
+		Rating              float64           `json:"rating"`
+		Reference           string            `json:"reference"`
+		Types               []string          `json:"types"`
+		UserRatingsTotal    int               `json:"user_ratings_total"`
+	}
+)
+
+func GeocodeReverse(req GeocodeReverseRequest) (*ResponseGeocodeReverse, error) {
 	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&key=%s", req.Lat, req.Long, config.GMAPS_API)
 	method := "GET"
 
@@ -90,7 +162,7 @@ func GeocodeReverse(req GeocodeReverseRequest) (*Response, error) {
 		return nil, err
 	}
 
-	var respBody Response
+	var respBody ResponseGeocodeReverse
 	if err := json.Unmarshal(body, &respBody); err != nil {
 		log.Printf("[GeocodeReverse] Error unmarshaling data, cause: %v", err)
 		return nil, err
@@ -99,7 +171,7 @@ func GeocodeReverse(req GeocodeReverseRequest) (*Response, error) {
 	return &respBody, nil
 }
 
-func GeocodeReverseWithFilter(req GeocodeReverseRequest) (*Response, error) {
+func GeocodeReverseWithFilter(req GeocodeReverseRequest) (*ResponseGeocodeReverse, error) {
 	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&result_type=street_address&key=%s", req.Lat, req.Long, config.GMAPS_API)
 	method := "GET"
 
@@ -123,7 +195,43 @@ func GeocodeReverseWithFilter(req GeocodeReverseRequest) (*Response, error) {
 		return nil, err
 	}
 
-	var respBody Response
+	var respBody ResponseGeocodeReverse
+	if err := json.Unmarshal(body, &respBody); err != nil {
+		log.Printf("[GeocodeReverse] Error unmarshaling data, cause: %v", err)
+		return nil, err
+	}
+
+	return &respBody, nil
+}
+
+// url = https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Rumah%20Sakit%20Dirgahayu&inputtype=textquery&fields=place_id,formatted_address,name,geometry&key=AIzaSyDQYavEeONw69aOFzYOe2l7XZlPnNnY7vE
+
+func FindPlace(req string) (*ResponseFindPlace, error) {
+
+	url := fmt.Sprintf("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=%v&inputtype=textquery&fields=business_status,place_id,icon,icon_background_color,icon_mask_base_uri,opening_hours,reference,formatted_address,user_ratings_total,types,plus_code,price_level,rating,name,geometry,photos&key=%v", req, config.GMAPS_API)
+	method := "GET"
+
+	client := &http.Client{}
+	getReq, err := http.NewRequest(method, url, nil)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	resp, err := client.Do(getReq)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var respBody ResponseFindPlace
 	if err := json.Unmarshal(body, &respBody); err != nil {
 		log.Printf("[GeocodeReverse] Error unmarshaling data, cause: %v", err)
 		return nil, err
